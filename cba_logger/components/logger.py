4bbc6e4567cbed85d9cbfa220274032f4b13bd54
Matthew van der Hoorn <md.registrations@protonmail.com>
2024-12-15 14:58:42 +0100

    Added Configurable Max Separator Length

--- cba_logger/components/logger.py
+++ cba_logger/components/logger.py
@@ -24,18 +24,30 @@ def get_log_dir(application_name: str):
 	return log_dir
 
 if __name__ == "__main__":
-	args = sys.argv
+	args = sys.argv[1:]
 
-	if len(args) != 2:
-		print(f"Usage: {args[0]} [Application Name Here]")
-		print(f"Example: {args[0]} 'Chess Player'")
+	if len(args) < 1:
+		print(f"Usage: {sys.argv[0]} [Application Name Here]")
+		print(f"Example: {sys.argv[0]} 'Chess Player'")
 		sys.exit(1)
 
-	application_name = args[1]
+	max_separator_length = 30
+
+	# noinspection PyBroadException
+	try:
+		if not args[1].startswith('-max_separator_length='):
+			raise ValueError("Invalid argument format. Expected '-max_separator_length=<value>'.")
+		v = int(args[1].split('=')[1])
+		max_separator_length = v
+		print(f"Using max separator length: {max_separator_length}")
+	except:
+		print("No max separator length provided. Using default 30.")
+
+	application_name = args[0]
 	dir = get_log_dir(application_name)
 	root_separator = application_name
 
-	logger = HoornLogger(min_level=LogType.DEBUG, outputs=[DefaultHoornLogOutput(), FileHoornLogOutput(max_logs_to_keep=5, log_directory=Path(dir))], separator_root=root_separator)
+	logger = HoornLogger(min_level=LogType.DEBUG, outputs=[DefaultHoornLogOutput(max_separator_length=max_separator_length), FileHoornLogOutput(max_logs_to_keep=5, log_directory=Path(dir), max_separator_length=max_separator_length)], separator_root=root_separator, max_separator_length=max_separator_length)
 
 	registration = ComponentRegistration(logger, port=50000, component_port=50001)
 	registration.register_logging()
